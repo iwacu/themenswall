cdcf6853704f46f5ab7092b5523cfb382e20fcb4
phonemgt <rutayisirewilly9@gmail.com>
2021-04-05 18:52:43 +0200

    smt

--- themenswall/settings.py
+++ themenswall/settings.py
@@ -144,4 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mikcraft1995@gmail.com'
-EMAIL_HOST_PASSWORD = 'bethefirst'
+EMAIL_HOST_PASSWORD = 'irdcudexygqgvjzw'
