47885d195265759a7a926f0e50a65cb1fa826e65
phonemgt <rutayisirewilly9@gmail.com>
2021-05-04 09:49:33 +0200

    ok help me lord

--- themenswall/settings.py
+++ themenswall/settings.py
@@ -135,16 +135,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'live-static','static-root')
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static', 'media-root')
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'live-static','static-root')
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static', 'media-root')
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'live-static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media-root')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
