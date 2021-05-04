694eec9ab3b25b8b2abb1415d955cbbfa58f12f9
phonemgt <rutayisirewilly9@gmail.com>
2021-05-04 07:26:51 +0200

    Postgresql detals

--- themenswall/settings.py
+++ themenswall/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'themenswall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'themenswall',
+        'USER': 'themenswall_admin',
+        'PASSWORD': 'Bethefirst@123#321',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
