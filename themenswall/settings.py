127e4153c686ffd245b16b7e5a6fc699edb2c71a
phonemgt <rutayisirewilly9@gmail.com>
2021-05-04 09:03:39 +0200

    ok

--- themenswall/settings.py
+++ themenswall/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'live-static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'live-static','static-root')
