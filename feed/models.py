0602dfb3224dfca147556c47d386984ff3e4f33a
phonemgt <rutayisirewilly9@gmail.com>
2021-05-05 10:32:06 +0200

    koodos

--- feed/models.py
+++ feed/models.py
@@ -7,7 +7,7 @@ from .validators import file_size
 class Post(models.Model):
 	description = models.CharField(max_length=255, blank=True)
 	pic = models.ImageField(upload_to='path/to/img',blank=True)
-	video = models.FileField(upload_to='path/to/img',validators=[file_size],blank=True)
+	video = models.FileField(upload_to='path/to/video',validators=[file_size],blank=True)
 	date_posted = models.DateTimeField(default=timezone.now)
 	user_name = models.ForeignKey(User, on_delete=models.CASCADE)
 	tags = models.CharField(max_length=100, blank=True)
